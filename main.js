b392ff80d935d8beabe7bb4dc9cb4273c8a23e26
wang-grace <wang.gra@northeastern.edu>
2023-02-27 22:47:21 -0500

    finished brushing

--- main.js
+++ main.js
@@ -23,58 +23,6 @@ const Y_SCALE1 = d3.scaleLinear()
                     .domain([0, 7])
                     .range([VIS_HEIGHT, 0]); 
 
-// builds scatter plot for length
-function build_length_scatter() {
-    // reads in data from file
-    d3.csv("data/iris.csv").then((data) => {
-
-        // add our circles with styling 
-        FRAME1.selectAll("point") 
-            .data(data)
-            .enter()  
-            .append("circle")
-                .attr("cx", (d) => { return (X_SCALE1(d.Sepal_Length) + MARGINS.left); }) // scaled X
-                .attr("cy", (d) => { return (Y_SCALE1(d.Petal_Length) + MARGINS.bottom); }) // scaled Y
-                .attr("r", 5)
-                .attr("class", (d) => { return d.Species; });
-                // .on("mouseover", function(d) {
-                //     d3.select(this).style("fill", "rosybrown"); }) // mouse over function       
-                // .on("mouseout", function(d) {
-                //     d3.select(this).style("fill", "pink"); }) // mouse out function
-                // .on("click", function(d) {
-                //     d3.select(this).classed("border", !d3.select(this).classed("border")); // toggles border class
-                //     click(d3.select(this).attr('cx'),  d3.select(this).attr("cy")); // displays last pt clicked
-            //})
-
-        // Add an X axis to the vis  
-        FRAME1.append("g") 
-            .attr("transform", "translate(" + MARGINS.left + 
-                "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-            .call(d3.axisBottom(X_SCALE1).ticks(10)) 
-                .attr("font-size", '10px'); 
-
-        // Add a Y axis to the vis  
-        FRAME1.append("g") 
-                .attr("transform", "translate(" + MARGINS.left + 
-                    "," + (MARGINS.top) + ")") 
-                .call(d3.axisLeft(Y_SCALE1).ticks(10)) 
-                    .attr("font-size", '10px');
-
-        FRAME1.append('text')
-                .attr("x", (FRAME_WIDTH / 2))             
-                .attr("y", (MARGINS.top / 2))
-                .attr("text-anchor", "middle")  
-                .style("font-size", "16px") 
-                .style("font-weight", "800")  
-                .text("Petal_Length vs. Sepal_Length");
-
-  
-    });
-};
-build_length_scatter();
-
-
-
 // WIDTH SCATTER PLOT
 // sets frame2 up
 const FRAME2 = d3.select("#scatterWidth") 
@@ -91,20 +39,57 @@ const Y_SCALE2 = d3.scaleLinear()
                     .domain([0, 3])
                     .range([VIS_HEIGHT, 0]); 
 
-// builds scatter plot for width
-function build_width_scatter() {
+// BAR PLOT
+const FRAME3 = d3.select("#bar") 
+                  .append("svg") 
+                    .attr("height", FRAME_HEIGHT)   
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame");
+
+
+
+// builds scatter plot for length
+function build_plots() {
     // reads in data from file
     d3.csv("data/iris.csv").then((data) => {
 
         // add our circles with styling 
-        FRAME2.selectAll("point") 
+        let myPoint1 = FRAME1.append("g")
+            .selectAll("point") 
+            .data(data)
+            .enter()  
+            .append("circle")
+                .attr("cx", (d) => { return (X_SCALE1(d.Sepal_Length) + MARGINS.left); }) // scaled X
+                .attr("cy", (d) => { return (Y_SCALE1(d.Petal_Length) + MARGINS.bottom); }) // scaled Y
+                .attr("r", 5)
+                .attr("class", (d) => { return d.Species + " point"  });
+
+        // Add an X axis to the vis  
+        FRAME1.append("g") 
+            .attr("transform", "translate(" + MARGINS.left + 
+                "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+            .call(d3.axisBottom(X_SCALE1).ticks(10)) 
+                .attr("font-size", '10px'); 
+
+        // Add a Y axis to the vis  
+        FRAME1.append("g") 
+                .attr("transform", "translate(" + MARGINS.left + 
+                    "," + (MARGINS.top) + ")") 
+                .call(d3.axisLeft(Y_SCALE1).ticks(10)) 
+                    .attr("font-size", '10px');
+
+
+
+        // add our circles with styling 
+        let myPoint2 = FRAME2.append("g")
+            .selectAll("point") 
             .data(data)
             .enter()  
             .append("circle")
                 .attr("cx", (d) => { return (X_SCALE2(d.Sepal_Width) + MARGINS.left); }) // scaled X
                 .attr("cy", (d) => { return (Y_SCALE2(d.Petal_Width) + MARGINS.bottom); }) // scaled Y
                 .attr("r", 5)
-                .attr("class", (d) => { return d.Species; });
+                .attr("class", (d) => { return d.Species + " point" });
 
         // Add an X axis to the vis  
         FRAME2.append("g") 
@@ -120,78 +105,40 @@ function build_width_scatter() {
                 .call(d3.axisLeft(Y_SCALE2).ticks(10)) 
                     .attr("font-size", '10px');
 
-        FRAME2.append('text')
-                .attr("x", (FRAME_WIDTH / 2))             
-                .attr("y", (MARGINS.top / 2))
-                .attr("text-anchor", "middle")  
-                .style("font-size", "16px") 
-                .style("font-weight", "800")  
-                .text("Petal_Width vs. Sepal_Width");
 
         FRAME2.call(d3.brush()
-                        .extent([MARGINS.left, MARGINS.top], [MARGINS.left + VIS_WIDTH, MARGINS.top + VIS_HEIGHT])
-                        .on("brush", updateChart));
+                        .extent([[0,0], [FRAME_WIDTH, FRAME_HEIGHT]])
+                        .on("start brush", updateChart));
 
         
-
-  
-    });
-};
-
-
-// Function that is triggered when brushing is performed
-function updateChart() {
-    extent = d3.event.selection
-    FRAME2.selectAll('circle').classed("brushed", function(d){ return isBrushed(extent, (X_SCALE2(d.Sepal_Width) + MARGINS.left), (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) ) } )
-}
-
-// A function that return TRUE or FALSE according if a dot is in the selection or not
-function isBrushed(brush_coords, cx, cy) {
-    var x0 = brush_coords[0][0],
-        x1 = brush_coords[1][0],
-        y0 = brush_coords[0][1],
-        y1 = brush_coords[1][1];
-    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
-}
-build_width_scatter();
-// BAR GRAPH
-// sets frame3 up
-const FRAME3 = d3.select("#bar") 
-                  .append("svg") 
-                    .attr("height", FRAME_HEIGHT)   
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame");
-
-function countSpecies(data, species) {
-    
-}
-
-// builds bar graph
-function build_bar() {
-    // reads in data from file
-    d3.csv("data/iris.csv").then((data) => {
-        // sets domains
+        // scaling x function for bar graph
         const X_SCALE3 = d3.scaleBand()
                             .domain(data.map(function (d) { return d.Species; }))
                             .range([0, VIS_WIDTH]).padding(0.2);
+
+        // scaling y function for bar graph
         const Y_SCALE3 = d3.scaleLinear()
                             .domain([0, 60])
                             .range([VIS_HEIGHT, 0]);
 
+        const data2 = [{Species: "setosa", Amount: 50},
+                {Species: "versicolor", Amount: 50},
+                {Species: "virginica", Amount: 50},
+            ]
+
+
         // add bars
-        FRAME3.selectAll("bar") 
-            .data(data)
+        let myBars = FRAME3.append("g")
+            .selectAll("bar") 
+            .data(data2)
             .enter()  
             .append("rect")
                 .attr("x", (d) => { return (X_SCALE3(d.Species) + MARGINS.left)})
-                .attr("y", (d) => { return (Y_SCALE3(50) + MARGINS.top)}) 
+                .attr("y", (d) => { return (Y_SCALE3(d.Amount) + MARGINS.top)}) 
                 .attr("width", X_SCALE3.bandwidth())
-                .attr("height", (d) => { return (VIS_HEIGHT - Y_SCALE3(50)); })
-                .attr("class", (d) => { return d.Species; });
-                // .on("mouseover", function(d) {
-                //     d3.select(this).style("fill", "rosybrown"); }) // mouse over function       
-                // .on("mouseout", function(d) {
-                //     d3.select(this).style("fill", "pink"); }); // mouse out function
+                .attr("height", (d) => { return (VIS_HEIGHT - Y_SCALE3(d.Amount)); })
+                .attr("class", (d) => { return d.Species + " bar" });
+
 
         // Add an X axis to the vis  
         FRAME3.append("g") 
@@ -207,14 +154,28 @@ function build_bar() {
                 .call(d3.axisLeft(Y_SCALE3).ticks(10)) 
                     .attr("font-size", '10px');
 
-        FRAME3.append('text')
-                .attr("x", (FRAME_WIDTH / 2))             
-                .attr("y", (MARGINS.top / 2))
-                .attr("text-anchor", "middle")  
-                .style("font-size", "16px") 
-                .style("font-weight", "800")  
-                .text("Counts of Species");
+
+// Function that is triggered when brushing is performed
+function updateChart(event) {
+    extent = event.selection;
+    myPoint1.classed("brushed", function(d){ return isBrushed(extent, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.top)  } )
+    myPoint2.classed("brushed", function(d){ return isBrushed(extent, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.top)  } )
+    myBars.classed("brushed", function(d){ return isBrushed(extent, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.top)  } )
+}
 
     });
 };
-build_bar();
+
+
+// A function that return TRUE or FALSE according if a dot is in the selection or not
+function isBrushed(brush_coords, cx, cy) {
+    var x0 = brush_coords[0][0],
+        x1 = brush_coords[1][0],
+        y0 = brush_coords[0][1],
+        y1 = brush_coords[1][1];
+    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
+}
+
+build_plots();
+
+
